c1bf35457ad0e72fb8fbab21537a7ca2803fcff8
Luit van Drongelen <me@luit.it>
2011-10-21 18:28:03 +0200

    Log errors with ListenAndServe.

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"http"
 	"http/cgi"
+	"log"
 	"os"
+	"strconv"
 )
 
 var (
@@ -14,9 +16,15 @@ var (
 		"Location of cgit resources.")
 	config = flag.String("config", "",
 		"Location of the cgit configuration file.")
+
+	port = flag.Int("port", 5000, 
+		"TCP port to listen on.")
+	addr = flag.String("addr", "127.0.0.1",
+		"Address to listen on.")
 )
 
 func main() {
+	log.SetFlags(0)
 	flag.Parse()
 
 	cgiHandler := &cgi.Handler{
@@ -30,15 +38,19 @@ func main() {
 		cgiHandler.Env = append(cgiHandler.Env,
 			"CGIT_CONFIG="+*config)
 	}
+
 	fs := http.FileServer(http.Dir(*cgitRes))
 	http.Handle("/cgit.css", fs)
 	http.Handle("/cgit.png", fs)
 	http.Handle("/", cgiHandler)
  
+	err := http.ListenAndServe(*addr + ":" + strconv.Itoa(*port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Everything seems to work: daemonize (close file handles)
 	os.Stdin.Close()
 	os.Stdout.Close()
 	os.Stderr.Close()
-
-	http.ListenAndServe("127.0.0.1:5000", nil)
 }
